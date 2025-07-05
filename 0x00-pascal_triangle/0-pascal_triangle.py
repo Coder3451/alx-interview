f494db8ae2b189e5b0a04a39fa6f01b637869d11
Coder3451 <segniwoldemichael@gmail.com>
2025-07-05 15:12:54 +0300

    Finish task 0-pascal_triangle.py

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,18 @@
+def pascal_triangle(n):
+    pasc_triangle = []
+
+    for i in range(n):
+        if i == 0:
+            pasc_triangle.append([1])
+        else:
+            previous_row = pasc_triangle[-1]
+            new_row = [1]
+
+            for j in range(1, len(previous_row)):
+                new_number = previous_row[j - 1] + previous_row[j]
+                new_row.append(new_number)
+
+            new_row.append(1)
+            pasc_triangle.append(new_row)
+
+    return pasc_triangle
